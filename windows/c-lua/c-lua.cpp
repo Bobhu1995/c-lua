8fa6cdcefa59263dcd2125c925ca282a719eea72
bobhu <bobhu@123u.com>
2020-12-07 16:03:04 +0800

    windows c-lua数据交互环境

---
+++ windows/c-lua/c-lua.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string.h>
+
+extern "C"
+{
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
+
+int main()
+{
+    lua_State* L = luaL_newstate();
+
+    lua_pushstring(L, "I am bob.");
+    lua_pushnumber(L, 20);
+
+    if (lua_isstring(L, 1))
+    {
+        std::cout << lua_tostring(L, 1) << std::endl;
+    }
+
+    if (lua_isnumber(L, 2))
+    {
+        std::cout << lua_tonumber(L, 2) << std::endl;
+    }
+
+    lua_close(L);
+    return 0;
+}
